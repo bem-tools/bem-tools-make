253c99f4da4597ada27d1b6b8e928fddc2be045a
Vladimir Grinenko <i@tadatuta.com>
2015-12-12 11:29:27 +0300

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+//var enb = require('enb');
+
+module.exports = function() {
+    this
+        .title('Make').helpful()
+        .act(function(opts, args) {
+            enb.make(process.argv.slice(3));
+        })
+        .end();
+};
